d63026099c4f5b0304226fc390798a90ad3fed33
Thebestavenger <thebestavenger2004@gmail.com>
2024-10-20 14:31:14 +0200

    add

---
+++ Map/Test/ShipDeco.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="ShipDeco" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../assets/Tileset/Tileset_Deco_Ship_1.png" width="256" height="256"/>
+</tileset>
